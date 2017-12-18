28e3c711f9e5614347635a793ebeacf06f12ce51
Terrance <terrancekoar@gmail.com>
2017-12-18 18:07:50 -0500

    update find_followers_for method
    update twitter_spec for find_followers_for method to be more explicit in what the test is expecting the output to be

--- spec/twitter_spec.rb
+++ spec/twitter_spec.rb
@@ -1,11 +1,11 @@
- require_relative 'spec_helper'
+require_relative 'spec_helper'
  require 'pry'
  require 'vcr'
 
 describe TwitterApi do
   let(:client) {TwitterApi.new}
 
-  describe "#most_recent_friend" do
+ describe "#most_recent_friend" do
     it "returns the most recent friend" do
       VCR.use_cassette('twitter/most_recent_friend') do
         user = client.most_recent_friend
@@ -15,8 +15,8 @@ describe TwitterApi do
     end
   end
 
-  describe "#find_user_for" do
-    it "given a username, it returns the user object" do
+ describe "#find_user_for" do
+    it "given a username, it returns the Twitter user object" do
       VCR.use_cassette('twitter/find_user_for') do
         user = client.find_user_for("sm_debenedetto")
         expect(user.class).to eq(Twitter::User)
@@ -25,8 +25,8 @@ describe TwitterApi do
     end
   end
 
-  describe "#find_followers_for" do
-    it "given a username, it returns the first 10 followers for that user" do
+ describe "#find_followers_for" do
+    it "given a username, it returns an array of the first 10 followers, with each follower's data represented in a hash" do
       VCR.use_cassette('twitter/find_followers_for') do
         user = client.find_followers_for("sm_debenedetto")
         expect(user).to be_a(Array)
@@ -37,7 +37,7 @@ describe TwitterApi do
     end
   end
 
-  describe "#homepage_timeline" do
+ describe "#homepage_timeline" do
     it "returns an array of tweet objects from the client user's homepage" do
       VCR.use_cassette('twitter/homepage_timeline') do
         tweet = client.homepage_timeline
